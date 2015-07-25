e8e956ff2622e0bd94fadb7b4308291890938f61
Jeff Rhodes <jeffrho68@gmail.com>
2015-07-25 12:21:55 -0500

    Leson 2: Connecting the app to the cloud to get data. Also updating min sdk version.

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,19 +1,9 @@
 package com.example.android.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -24,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -51,39 +41,5 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
 
-        ArrayAdapter<String> mForecastListAdapter;
-        ListView mForecastListView;
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            String[] strForecasts = {"Today - Sunny - 88/63",
-                    "Tommorow - Foggy - 70/46",
-                    "Weds - Cloudy - 72/63",
-                    "Thurs - Rainy - 64/41",
-                    "Fri - Foggy - 70/46",
-                    "Sat - Sunny - 76/68",
-                    "Sun - Sunny - 82/66"};
-
-            List<String> forecasts = Arrays.asList(strForecasts);
-
-            mForecastListAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast_textview, forecasts);
-            mForecastListView = (ListView)rootView.findViewById(R.id.listview_forecast);
-            mForecastListView.setAdapter(mForecastListAdapter);
-
-
-
-            return rootView;
-        }
-    }
 }
